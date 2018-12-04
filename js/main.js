618d8d86ec1b453792172666bade6b56a63f4c83
Abdul Rehman <abdul-rehman@myseneca.ca>
2018-12-03 18:38:32 -0600

    itteration 3 layout

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     console.log("document ready!");
+// });
+
+// alternatively:
+
+$(function() {
+   console.log( "document ready!" );``
+
+});
+
+console.log("file loaded");
