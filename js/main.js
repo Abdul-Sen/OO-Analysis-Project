d6d40500dfdba8b195311377a46b10218b9d6d55
Abdul Rehman <abdul-rehman@myseneca.ca>
2018-12-04 18:07:47 -0600

    Iteration 3

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-// $(document).ready(function(){
-//     console.log("document ready!");
-// });
-
-// alternatively:
-
 $(function() {
    console.log( "document ready!" );
-   // $('.covervid-video').coverVid(720, 1080);
+   $('.covervid-video').coverVid(720, 1080);
 });
 
 console.log("file loaded");
+
+/*
+ASK:
+>>>>>>>Vertical alignment in bootstrap
+Using bootstrap jQuery and bootstrap CSS classes?
+Glyph icons vs i tag?
+HOW TO CENTER HEADINGS!!!!!!
+
+*/
+/*
+IDEA:
+1- Change video opency to 70% so its faded in
+2- Put a heading there
+3- Change the #first margin so it fits
+
+*/
